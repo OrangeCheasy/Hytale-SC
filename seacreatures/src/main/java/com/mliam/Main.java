703cbe5c12f6eedac1758cf178573159276ad5ed
LAM <96196827+OrangeCheasy@users.noreply.github.com>
2026-01-19 20:27:43 +0000

    Inital Commit

--- seacreatures/src/main/java/com/mliam/Main.java
+++ seacreatures/src/main/java/com/mliam/Main.java
@@ -1,5 +1,9 @@
 package com.mliam;
 
+import com.hypixel.hytale.server.core.plugin.JavaPlugin;
+import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
+
+
 public class Main extends JavaPlugin {
     public static void main(String[] args) {
         System.out.println("Hello world!");
